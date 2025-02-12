955d3b79eead9f42be4e62225a7209ae657553dd
santiagoinaki <santiagosoraluce@gmail.com>
2025-02-12 17:31:45 -0600

    Update block-npm-install.js

--- scripts/block-npm-install.js
+++ scripts/block-npm-install.js
@@ -1,12 +1,21 @@
 const { npm_config_user_agent: UA } = process.env;
 const [packageManager] = (UA ?? '').split(' ');
 const [name, version] = packageManager.split('/');
-if (name !== 'pnpm') {
-	const suggestion = '\033[1;92mpnpm\033[0;31m';
-	console.error('\033[0;31m');
-	console.error('╭───────────────────────────────────────────╮');
-	console.error(`│\tPlease use ${suggestion} instead of ${name} \t    │`);
-	console.error('╰───────────────────────────────────────────╯');
-	console.error('\033[0m');
-	process.exit(1);
+
+// Verifica si el comando es para instalar pnpm globalmente
+const isGlobalPnpmInstall = process.argv.some(arg => 
+  arg.includes('install') && 
+  arg.includes('-g') && 
+  arg.includes('pnpm@')
+);
+
+// Permite npm install -g pnpm@... o si hay una variable de entorno
+if (name !== 'pnpm' && !isGlobalPnpmInstall && process.env.ALLOW_NPM_INSTALL !== 'true') {
+  const suggestion = '\x1b[1;92mpnpm\x1b[0;31m';
+  console.error('\x1b[0;31m');
+  console.error('╭───────────────────────────────────────────╮');
+  console.error(`│\tPlease use ${suggestion} instead of ${name} \t    │`);
+  console.error('╰───────────────────────────────────────────╯');
+  console.error('\x1b[0m');
+  process.exit(1);
 }
